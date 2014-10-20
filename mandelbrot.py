41fff6140da73d4d9456b02e8f6369375e2e1939
bitsofbits <tim@bitsofbits.com>
2014-10-20 15:39:44 -0700

    Added usage instructions
    Added instructions on how to run the code using Python, PyPy and
    RPython. Also added some brief notes on the structure of the code.

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,4 +1,52 @@
+"""mandelbrot.py => madelbrot set in PGM format to stdout.
+
+This code writes out a Mandelbrot set to stdout based on the parameters x_center, 
+y_center, etc .defined below. This code isn't particularly sophisticated, rather it's
+meant to be used for speed comparisons with mandelbrot.mach, which is runs on the 
+bytecode interpreter machine.py, and which operates similarly.
+
+This runs using 
+    * Python2 (tested with 2.7.7):
+        $ python mandelbrot.py > m.pgm
+    * PyPy (tested with 2.3.1):
+        $ pypy mandelbrot.py > m.pgm
+    * Translated using RPython (tested with version from PyPy 2.3.1). You'll need the
+      source distribution for RPython and you'll need to adjust the paths appropriately. 
+        $ pypy rpython/bin/rpython mandelbrot.py 
+        $ ./mandelbrot-c > m.pgm
+
+"""
+import os
+import sys
 from math import hypot
+
+
+# Parameters defining the Mandelbrot set
+
+x_center = -0.5
+y_center = 0.0
+width = 4.0
+i_max = 1600
+j_max = 1200
+n = 100
+r_max = 2.0
+colour_max = 255
+
+scale = float(colour_max) / n
+pixel_size = width / i_max
+x_offset = x_center - (0.5 * pixel_size * (i_max + 1))
+y_offset = y_center - (0.5 * pixel_size * (j_max + 1))
+
+
+# Write our own print-like function that RPython likes.
+
+stdout_fd = sys.stdout.fileno()
+
+def write(text):
+    os.write(stdout_fd, text)
+
+
+# This is the core of the program. Not much too it
 
 def iterate_z(re0, im0, re, im, n):
     for i in range(n):
@@ -24,26 +72,8 @@ def generate_pgm():
         for i in range(i_max):
             write("%s\n" % pixel(i,j))
 
-# Setup code, etc...
-    
-import os, sys
-stdout_fd = sys.stdout.fileno()
 
-def write(text):
-    os.write(stdout_fd, text)
-    
-x_center = -0.5
-y_center = 0.0
-width = 4.0
-i_max = 1600
-j_max = 1200
-n = 100
-r_max = 2.0
-colour_max = 255
-scale = float(colour_max) / n
-pixel_size = width / i_max
-x_offset = x_center - (0.5 * pixel_size * (i_max + 1))
-y_offset = y_center - (0.5 * pixel_size * (j_max + 1))
+# Startup boilerplate. 'target' is so that RPython knows where to start translating.
     
 def main(argv):
     generate_pgm()
